d72b254ed35d8d356c6f3562db838a9979387467
Raphael Rivas <raphaelrivas@hotmail.com>
2025-06-26 00:37:26 -0300

    test: failed test

--- tests/hello-world.spec.ts
+++ tests/hello-world.spec.ts
@@ -4,5 +4,5 @@ import { test, expect } from '@playwright/test';
 test('basic test - check title of example.com', async ({ page }) => {
     await page.goto('https://example.com');
     const title = await page.title();
-    expect(title).toBe('Example Domain');
+    expect(title).toBe('Example Domain1');
 });
