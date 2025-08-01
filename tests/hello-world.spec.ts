c355bf5335a74b192b6c3e4d227eb05fddc9e667
Raphael Rivas <raphaelrivas@hotmail.com>
2025-08-01 05:40:54 -0300

    more tests

--- tests/hello-world.spec.ts
+++ tests/hello-world.spec.ts
@@ -8,3 +8,19 @@ test('basic test - check title of example.com', async ({ page }) => {
     // Simply test if true is true
     expect(true).toBe(true);
 });
+
+test('basic test - 2', async ({ page }) => {
+    // await page.goto('https://example.com');
+    // const title = await page.title();
+    // expect(title).toBe('Example Domain');
+    // Simply test if true is true
+    expect(true).toBe(true);
+});
+
+test('basic test - 3', async ({ page }) => {
+    // await page.goto('https://example.com');
+    // const title = await page.title();
+    // expect(title).toBe('Example Domain');
+    // Simply test if true is true
+    expect(true).toBe(false);
+});
